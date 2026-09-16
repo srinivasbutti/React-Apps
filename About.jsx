d9b7d3d4c1b9104a0d9877d71d3723f568b898ec
SRINIVAS BUTTI <93898558+srinivasbutti@users.noreply.github.com>
2026-09-16 13:03:14 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About=()=>{
+    return(
+
+         <div style={{ textAlign: "right", marginTop: "150px", }}>
+            <h2 style={{color:'blue'}}>About Page</h2>
+        </div>
+        )
+}
+export default About;
---
+++ Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home=()=>{
+    return(
+           <div style={{ textAlign: "left", marginTop: "150px", }}>
+            <h2 style={{color:'red'}}>Home Component</h2>
+       </div>
+    )
+}
+
+export default Home;
